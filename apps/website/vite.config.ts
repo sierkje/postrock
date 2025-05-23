1d603087a48b2826b07d3bf0671d882bc5739350
Sierk <6021957+sierkje@users.noreply.github.com>
2025-05-23 15:44:10 -0700

    Initial commit

---
+++ apps/website/vite.config.ts
@@ -0,0 +1,14 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
+  plugins: [tailwindcss(), reactRouter()],
+}));
